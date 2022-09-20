2b8aeb4250b839aa8781d7bf2deff17d92a65881
Vu Nguyen <aqaurius6666@gmail.com>
2022-09-20 21:04:37 +0700

    check

--- deply.sh
+++ deply.sh
@@ -1,7 +1,7 @@
 OWNER=aqaurius6666
 PACKAGE_DIR=.deploy
 REPOSITORY_NAME=helm-charts
-GH_TOKEN=
+# GH_TOKEN=
 CHART_PATH=charts/sish-client
 # helm package $CHART_PATH --destination $PACKAGE_DIR
 # cr index --owner $OWNER -p $PACKAGE_DIR -r $REPOSITORY_NAME -t $GH_TOKEN -i index.yaml
